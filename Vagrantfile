671899d81390633ce176c71574273bf0692ea42a
Gabriel Arellano <arellanog@cent.gov.ar>
2017-07-23 12:49:10 -0300

    Agrego Vagranfile y archivos de provisioning

---
+++ Vagrantfile
@@ -0,0 +1,30 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # Base box de la que partimos
+  config.vm.box = "ubuntu/trusty32"
+
+  # Configuramos que el subirectorio ./practicas en el host que esté mapeado en el home de la máquina virtual
+  config.vm.synced_folder "./practicas", "/home/vagrant/practicas", create: true, group: "vagrant", owner: "vagrant"
+  
+  # Configuración específica para el provider virtualbox
+  config.vm.provider "virtualbox" do |v|
+      # Seteamos el nombre de la máquina virtual
+      v.name = "S.O. 2017 (32 bits)"
+      # Cambiamos la memoria dedicada a la máquina virtual
+      v.customize ["modifyvm", :id, "--memory", "256"]
+      # Activamos el portapapeles entre el host y la máquina virtual
+      v.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+  end
+
+  # Configuranmos el provisioning mediante shell script
+  config.vm.provision "shell" do |s|
+      s.path = "provision/setup.sh"
+  end
+
+end
---
+++ provision/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Seteamos el modo no interactivo de apt-get
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Provisionando la máquina virtual..."
+
+#Cambiando el hostname a la VM...
+hostnamectl set-hostname so-2017
+sed -i 's/localhost/localhost so-2017/g' /etc/hosts
+
+echo "Actualizando lista de paquetes..."
+# Actualizo la lista de paquetes
+apt-get update > /dev/null
+
+echo "Ajustando locales e idioma..."
+# Ajusto el locale por default a es_ES
+locale-gen es_ES es_ES.UTF-8 > /dev/null
+update-locale LANG=es_ES.utf-8 > /dev/null
+
+echo "Instalando páginas man..."
+# Instalo manuales en español
+apt-get install manpages-es manpages-es-extra > /dev/null
+# Instalo manuales utiles en las prácticas de la materia
+apt-get install manpages-posix manpages-posix-dev manpages-dev > /dev/null
+
+echo "Instalando Herramientas Básicas..."
+# gpm para dar soporte al mouse en línea de comandos
+# htop: un comando top mejorado
+apt-get install gpm htop -y > /dev/null
+
+echo "Descargando FreePascal 2.4.0..."
+# Creamos un directorio para los archivos temporales
+mkdir /tmp/fpc
+cd /tmp/fpc
+# Descargamos y descomprimimos FreePascal 2.4.0 32 bits
+wget -q https://sourceforge.net/projects/freepascal/files/Linux/2.4.0/fpc-2.4.0.i386-linux.tar
+
+echo "Instalando FreePascal 2.4.0..."
+# Desempaquetamos FreePascal
+tar xvf fpc-2.4.0.i386-linux.tar > /dev/null
+# Instalamos...
+./install.sh < /vagrant/provision/fpc/install_opts > /dev/null
+cd
+# Eliminamos archivos temporales
+rm -rf /tmp/fpc
+# Copiamos archivos de configuración de FreePascal IDE
+cp /vagrant/provision/fpc/fp.* /home/vagrant/
+chown vagrant.vagrant /home/vagrant/fp.*
+
+# Forzamos el color en el prompt de la máquina virtual
+sed -i 's/#force_color_prompt/force_color_prompt/g' /home/vagrant/.bashrc
+
+echo "Finalizadas las actividades de instalación y configuración!"
